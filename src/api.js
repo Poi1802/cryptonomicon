94f9369e1aa2b40666d40f40fd2f499d369ca31b
Poi1802 <you@example.com>
2023-03-04 20:48:10 +0800

    made multi-page update with localStorage1

--- src/api.js
+++ src/api.js
@@ -3,6 +3,8 @@ const AGREGGATE_IDX = '5';
 
 const tickersHadler = new Map();
 
+const tickersData = JSON.parse(localStorage.getItem('crypto-list'));
+
 const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`);
 
 socket.addEventListener('message', (e) => {
@@ -11,6 +13,24 @@ socket.addEventListener('message', (e) => {
   if (type === AGREGGATE_IDX && newPrice) {
     const ticker = tickersHadler.get(coin) || [];
     ticker.forEach((fn) => fn(newPrice));
+
+    tickersData.forEach((obj) => {
+      if (obj.name === coin) {
+        obj.price = newPrice;
+      }
+    });
+
+    localStorage.setItem('crypto-list', JSON.stringify(tickersData));
+  } else if (type === '429') {
+    setInterval(() => {
+      const storageData = JSON.parse(localStorage.getItem('crypto-list'));
+      // console.log(storageData);
+
+      storageData.forEach((obj) => {
+        const ticker = tickersHadler.get(obj.name) || [];
+        ticker.forEach((fn) => fn(obj.price));
+      });
+    }, 1000);
   }
 });
 
